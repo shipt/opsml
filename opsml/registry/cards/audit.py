d84c143d95118995a1261a5d53761050147af865
Rohan Yedagiri <rohankumar0068@gmail.com>
2023-06-29 15:33:43 -0500

    edit audit structure and load yaml file

--- opsml/registry/cards/audit.py
+++ opsml/registry/cards/audit.py
@@ -1,94 +1,166 @@
 # pylint: disable=too-many-lines
 
 from typing import Optional, List
-
+import yaml
 from pydantic import validator, BaseModel
 
 from opsml.registry.cards.base import ArtifactCard
 from opsml.registry.cards.types import CardType
 from opsml.registry.sql.records import ProjectRegistryRecord, RegistryRecord
 
+class Question:
+    def __init__(self, purpose: str, question: str):
+        self.purpose = purpose
+        self.question = question
+        self.response = None
 
-class Question(BaseModel):
-    question: str
-    purpose: str
-    response: Optional[str] = None
-
-
-class BaseAudit:
-    questions: List[Question]
-
-
-class DataUnderstanding(BaseAudit):
-    pass
+    def __repr__(self):
+        return f"Question(purpose={self.purpose}, question={self.question}, response={self.response})"
 
 
-class DataPreparation(BaseAudit):
-    pass
+class AuditSection:
+    def __init__(self, section_name: str):
+        self.section_name = section_name
+        self.questions = []
+
+    def add_question(self, purpose: str, question: str):
+        self.questions.append(Question(purpose, question))
+
+    def __repr__(self):
+        return f"AuditSection(section_name={self.section_name}, questions={self.questions})"
 
 
-class DataAudit(BaseModel):
-    understanding: DataUnderstanding
-    preparation: DataPreparation
+class AuditCard:
+    def __init__(self):
+        self.sections = {}
+
+    def add_section(self, section_name: str):
+        self.sections[section_name] = AuditSection(section_name)
+
+    def add_question_to_section(self, section_name: str, purpose: str, question: str):
+        if section_name not in self.sections:
+            self.add_section(section_name)
+        self.sections[section_name].add_question(purpose, question)
+
+    def collect_responses(self):
+        for section in self.sections.values():
+            for question in section.questions:
+                question.response = input(f"Question: {question.question} \n Purpose: {question.purpose}: \n")
+
+    def to_yaml(self, filename: str):
+        data = {}
+        for section_name, section in self.sections.items():
+            data[section_name] = {
+                "questions": [
+                    {
+                        "purpose": question.purpose,
+                        "question": question.question,
+                        "response": question.response
+                    }
+                    for question in section.questions
+                ]
+            }
+        with open(filename, "w") as file:
+            yaml.dump(data, file)
+
+if __name__ == "__main__":
+    audit_card = AuditCard()
+
+    # Load the YAML file and populate the AuditCard
+    with open("opsml/registry/cards/templates/audit.yaml") as file:
+        yaml_data = yaml.load(file, Loader=yaml.FullLoader)
+        for section_name, section_data in yaml_data.items():
+            for question_data in section_data:
+                audit_card.add_question_to_section(section_name, question_data["purpose"], question_data["question"])
+
+    # Collect user responses
+    audit_card.collect_responses()
+
+    # Output the audit report as YAML file
+    audit_card.to_yaml("opsml/registry/cards/templates/audit_responses.yaml")
 
 
-class ModelAudit(BaseAudit):
-    pass
+# class Question(BaseModel):
+#     question: str
+#     purpose: str
+#     response: Optional[str] = None
 
 
-class EvaluationAudit(BaseAudit):
-    pass
+# class BaseAudit:
+#     questions: List[Question]
 
 
-class DeploymentAudit(BaseAudit):
-    pass
+# class DataUnderstanding(BaseAudit):
+#     pass
 
 
-class AuditCard(ArtifactCard):
-    """
-    Card containing audit information
-    """
+# class DataPreparation(BaseAudit):
+#     pass
 
-    data: DataAudit
-    model: ModelAudit
-    evaluation: EvaluationAudit
-    deployment: DeploymentAudit
 
-    def load(self):
-        """Load section questions from templates
+# class DataAudit(BaseModel):
+#     understanding: DataUnderstanding
+#     preparation: DataPreparation
 
-        Maybe make templates with yaml?
 
-        i.e.:
+# class ModelAudit(BaseAudit):
+#     pass
 
-        data:
-            understanding:
-                - Question
-                  Purpose
-                  Response
-                - Question
-                  Purpose
-                  Response
-            preparation:
-                - Question
-                  Purpose
-                  Response
-                - Question
-                  Purpose
-                  Response
-        model:
-            - Question
-              Purpose
-              Response
-        evaluation:
-            - Question
-              Purpose
-              Response
-        deployment:
-            - Question
-              Purpose
-              Response
-        """
 
-    def score(self):
-        "Audit score"
+# class EvaluationAudit(BaseAudit):
+#     pass
+
+
+# class DeploymentAudit(BaseAudit):
+#     pass
+
+
+# class AuditCard(ArtifactCard):
+#     """
+#     Card containing audit information
+#     """
+
+#     data: DataAudit
+#     model: ModelAudit
+#     evaluation: EvaluationAudit
+#     deployment: DeploymentAudit
+
+#     def load(self):
+#         """Load section questions from templates
+
+#         Maybe make templates with yaml?
+
+#         i.e.:
+
+#         data:
+#             understanding:
+#                 - Question
+#                   Purpose
+#                   Response
+#                 - Question
+#                   Purpose
+#                   Response
+#             preparation:
+#                 - Question
+#                   Purpose
+#                   Response
+#                 - Question
+#                   Purpose
+#                   Response
+#         model:
+#             - Question
+#               Purpose
+#               Response
+#         evaluation:
+#             - Question
+#               Purpose
+#               Response
+#         deployment:
+#             - Question
+#               Purpose
+#               Response
+#         """
+
+#     def score(self):
+#         "Audit score"
+
