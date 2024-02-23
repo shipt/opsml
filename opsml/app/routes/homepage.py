edab52817b306bdf2f69f77b50e26d9390bc92dc
thorrester <steven.forrester@shipt.com>
2024-02-23 22:18:25 +0000

    starting spa

--- opsml/app/routes/homepage.py
+++ opsml/app/routes/homepage.py
@@ -4,24 +4,36 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 from pathlib import Path
-
+from typing import Optional
 from fastapi import APIRouter, Request
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 # Constants
 TEMPLATE_PATH = Path(__file__).parents[1] / "templates"
 templates = Jinja2Templates(directory=TEMPLATE_PATH)
 
-
-templates = Jinja2Templates(directory=TEMPLATE_PATH)
-
 router = APIRouter()
 
 
 @router.get("/opsml")
-async def opsml_homepage(request: Request) -> RedirectResponse:
-    return RedirectResponse(url="/opsml/models/list/")
+async def opsml_homepage(
+    request: Request,
+    registry: Optional[str] = None,
+    name: Optional[str] = None,
+    repository: Optional[str] = None,
+    version: Optional[str] = None,
+) -> HTMLResponse:
+    return templates.TemplateResponse(
+        "include/index.html",
+        {
+            "request": request,
+            "registry": registry,
+            "name": name,
+            "repository": repository,
+            "version": version,
+        },
+    )
 
 
 @router.get("/")
