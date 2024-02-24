575739b1de1244bdc197dd55c489d6e292005f9b
thorrester <steven.forrester@shipt.com>
2024-02-24 21:50:36 +0000

    ongoing

--- opsml/app/static/js/utils.js
+++ opsml/app/static/js/utils.js
@@ -164,7 +164,6 @@ function get_repo_names_page(registry, repository) {
         dataType:"json",
         data: uri_data,
         success: function(data) {
-            alert(JSON.stringify(data));
             // get repository and names from dictionary
             var repositories = data["repositories"];
             var names = data["names"];
@@ -189,6 +188,72 @@ function get_repo_names_page(registry, repository) {
                 opt.value = "No repositories found";
                 opt.innerHTML = "No repositories found";
                 select.appendChild(opt);
+            }
+
+            if (names.length > 0) {
+                alert("names found");
+
+                // if repository is undefined, set it to the first repository
+                if (repository == undefined) {
+                    repository = repositories[0];
+                }
+
+                var repo_header = document.getElementById("repository-header");
+                 // created heading
+                var repo_heading = document.createElement('h2');
+                repo_heading.innerHTML = repository;
+                repo_header.appendChild(repo_heading);
+
+
+                var artifact_card_div = document.getElementById("artifact-card-div");
+                artifact_card_div.innerHTML = "";
+            
+                for (var i = 0; i < names.length; i++)
+                {
+                    var card = document.createElement('div');
+                    card.className = "card text-left rounded m-1";
+                    card.style = "width: 14rem;";
+                    card.id = "artifact-card";
+            
+                    var card_body = document.createElement('div');
+                    card_body.className = "card-body";
+            
+                    var card_row = document.createElement('div');
+                    card_row.className = "row";
+            
+                    var card_col = document.createElement('div');
+                    card_col.className = "col-sm-8";
+            
+                    var card_title = document.createElement('h5');
+                    card_title.className = "card-title";
+                    card_title.innerHTML = names[i];
+            
+                    var card_text = document.createElement('a');
+                    card_text.className = "stretched-link";
+                    card_text.href = "#";
+                    card_text.value = names[i];
+            
+            
+                    card_col.appendChild(card_title);
+                    card_col.appendChild(card_text);
+                    card_row.appendChild(card_col);
+            
+                    // create image column
+                    var card_col_img = document.createElement('div');
+                    card_col_img.className = "col-sm-4";
+                    card_col_img.id = "artifact-card-img";
+            
+                    var card_img = document.createElement('img');
+                    card_img.className = "center-block";
+                    card_img.src = "/static/images/chip.png";
+            
+                    card_col_img.appendChild(card_img);
+                    card_row.appendChild(card_col_img);
+            
+                    card_body.appendChild(card_row);
+                    card.appendChild(card_body);
+                    artifact_card_div.appendChild(card);
+                }
             }
         },
 
