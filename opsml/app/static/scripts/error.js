2ce3700e66ee645a985528a675367a3237f647b1
thorrester <steven.forrester@shipt.com>
2024-03-01 18:55:37 +0000

    lint error

--- opsml/app/static/scripts/error.js
+++ opsml/app/static/scripts/error.js
@@ -1,29 +1,28 @@
-const ACTIVE_CARD_PATH = "/opsml/ui/error";
+import $ from 'jquery';
+
+const ACTIVE_CARD_PATH = '/opsml/ui/error';
 
 function errorToPage(message) {
+  const request = { message };
 
-    let request = {"message": message};
+  return $.ajax({
+    url: ACTIVE_CARD_PATH,
+    type: 'POST',
+    dataType: 'html',
+    contentType: 'application/json',
+    data: JSON.stringify(request),
+    success(data) {
+      document.open();
+      document.write(data);
+      document.close();
+    },
 
-    return $.ajax({
-        url: ACTIVE_CARD_PATH,
-        type: "POST",
-        dataType: "html",
-        contentType: "application/json",
-        data: JSON.stringify(request),
-        success: function(data) {
-
-            document.open();
-            document.write(data);
-            document.close();
-
-        },
-
-        error: function(xhr, status, error) {
-            // send request to error route on error
-            var err = JSON.parse(xhr.responseText);
-            alert(JSON.stringify(err));
-        }
-    });
+    error(xhr, status, error) { // eslint-disable-line no-unused-vars
+      // send request to error route on error
+      const err = JSON.parse(xhr.responseText);
+      alert(JSON.stringify(err)); // eslint-disable-line no-alert
+    },
+  });
 }
 
-export { errorToPage };
+export { errorToPage }; // eslint-disable-line import/prefer-default-export
