2b56aa8987a737eb646f46561ac2378df87a1f24
thorrester <steven.forrester@shipt.com>
2024-03-02 18:51:21 +0000

    version test

---
+++ opsml/app/static/scripts/typescript/version.test.ts
@@ -0,0 +1,18 @@
+import { createVersionElements, Card } from './version';
+
+test('test-create-version-elements', () => {
+  document.body.innerHTML = `
+        <div id="version-list"></div>
+        <div id="version-header"></div>
+    `;
+
+  const cardVersions: Card[] = [
+    {
+      version: 'test',
+      date: 'test',
+      repository: 'test',
+    },
+  ];
+
+  createVersionElements(cardVersions, 'test', 'test', 'test');
+});
--- opsml/app/static/scripts/typescript/version.ts
+++ opsml/app/static/scripts/typescript/version.ts
@@ -176,4 +176,5 @@ export {
   setVersionPage,
   getVersions,
   createVersionElements,
+  Card,
 };
