9f66d626fd2e8caa33a27e84c07314d1b8b1e33d
Thorrester <steven.forrester@shipt.com>
2024-03-16 08:05:08 -0400

    adding deps

--- opsml/app/static/scripts/src/routes/+page.svelte
+++ opsml/app/static/scripts/src/routes/+page.svelte
@@ -1,45 +1,12 @@
-<!-- src/routes/+page.svelte -->
+
+
 <script>
-    import Navbar from '$lib/Navbar.svelte'
-</script>
-
-<div class="home_hero">
-    <h1>Enjoy Static Site Hosting With Kinsta StSH.</h1>
-    <p>Fast, Secure, Reliable Hosting Solution.</p>
-    <a href="https://kinsta.com/static-site-hosting/">
-        <div class="btn">Read more</div>
-    </a>
-</div>
-
-<style>
-    .home_hero {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
-        text-align: center;
-    }
-    .home_hero h1 {
-        font-size: 60px;
-        width: 70%;
-    }
-    .home_hero p {
-        color: #6e7076;
-        font-size: 20px;
-    }
-    .btn {
-        background-color: #5333ed;
-        padding: 20px 30px;
-        margin-top: 40px;
-        border-radius: 5px;
-        color: #fff;
-    }
-    @media (max-width: 700px) {
-        .home_hero h1 {
-            font-size: 40px;
-        }
-        .home_hero p {
-            font-size: 16px;
-        }
-    }
-</style>
+    import { Alert } from 'flowbite-svelte';
+  </script>
+  
+  <div class="p-8">
+    <Alert>
+      <span class="font-medium">Info alert!</span>
+      Change a few things up and try submitting again.
+    </Alert>
+  </div>
--- opsml/app/static/scripts/svelte.config.js
+++ opsml/app/static/scripts/svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
 	kit: {
@@ -15,5 +16,6 @@ export default {
 			precompress: false,
 			strict: true
 		})
-	}
+	},
+	preprocess: vitePreprocess()
 };
