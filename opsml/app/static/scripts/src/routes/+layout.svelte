e5a539255c293a2a21b398d0940dfc445f55a44b
Thorrester <steven.forrester@shipt.com>
2024-03-17 09:00:12 -0400

    navbar

--- opsml/app/static/scripts/src/routes/+layout.svelte
+++ opsml/app/static/scripts/src/routes/+layout.svelte
@@ -2,7 +2,12 @@
     import "../app.css";
     import Navbar from '$lib/Navbar.svelte';
     import Footer from '$lib/Footer.svelte';
+    import favicon from '$lib/images/favicon.svg';
 </script>
+
+<svelte:head>
+  <link rel="icon" type="image/svg" href={favicon} />
+</svelte:head>
 
 <div class="layout">
     <Navbar />
