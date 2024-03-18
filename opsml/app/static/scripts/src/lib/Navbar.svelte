2c09b5fdad2b54bf66db5bdb3207a99980a6dbea
Thorrester <steven.forrester@shipt.com>
2024-03-17 21:48:45 -0400

    fix ignore

---
+++ opsml/app/static/scripts/src/lib/Navbar.svelte
@@ -0,0 +1,105 @@
+<script>
+  import { page } from '$app/stores';
+  import logo from '$lib/images/opsml_word.png';
+  import Fa from 'svelte-fa'
+  import { faFlag } from '@fortawesome/free-solid-svg-icons'
+
+  
+</script>
+
+<header class="shadow-[0_4px_0_0_black]">
+  
+  <div class="mx-auto px-4 container relative flex h-16 items-center">
+
+    <div class="flex flex-1 items-center">
+  
+      <a class="flex items-center lg:mr:6">
+        <img src={logo} class="h-5 sm:h-10" alt="Opsml Logo" />
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
+      </a>
+    </div>
+
+    <nav>
+      <ul class="flex items-center space-x-2 xl:space-x-3">
+
+        <li>
+          <a class="group flex items-center text-white text-base md:text-lg active:font-bold" href='/opsml/registry/model' class:active={$page.url.pathname === '/opsml/registry/model'}>
+            Model
+          </a>
+        </li>
+
+        <li>
+          <a class="group flex items-center text-white text-base md:text-lg" href='/opsml/registry/data' class:active={$page.url.pathname === '/opsml/registry/data'}>
+            Data
+          </a>
+        </li>
+
+        <li>
+          <a class="group flex items-center text-white text-base md:text-lg" href='/opsml/registry/run' class:active={$page.url.pathname === '/opsml/registry/run'}>
+            Runs
+          </a>
+        </li>
+
+        <li>
+          <a class="group flex items-center text-white text-base md:text-lg" href='/opsml/registry/audit' class:active={$page.url.pathname === '/opsml/registry/audit'}>
+            Audit
+          </a>
+        </li>
+
+        <li>
+          <a class="group flex items-center text-white text-base md:text-lg" href='/opsml/registry/audit' class:active={$page.url.pathname === '/opsml/registry/audit'}>
+            Docs
+          </a>
+        </li>
+
+      </ul>
+    </nav>
+    <div class="flex items-center border-l border-slate-200 ml-4 pl-2">
+      <a href="https://github.com/shipt/opsml" class="ml-2 block text-slate-400 hover:text-slate-500">
+        <span class="sr-only">Opsml on GitHub</span>
+        <svg viewBox="0 0 16 16" class="w-6 h-6 fill-white" aria-hidden="true">
+          <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+        </svg>
+      </a>
+    </div>
+  </div>
+</header>
+
+
+<style>
+
+  header {
+    background: rgb(75, 57, 120);
+
+  }
+
+  nav a:focus {
+    text-decoration: underline;
+    text-decoration-color: rgb(26, 255, 163);
+    text-underline-offset: 1em;
+    font-weight: 700;
+  }
+
+  
+  nav a:hover {
+    text-decoration: underline;
+    text-decoration-color: rgb(26, 255, 163);
+    text-underline-offset: 1em;
+    font-weight: 700;
+  }
+
+  nav a.active {
+    text-decoration: underline;
+    text-decoration-color: rgb(26, 255, 163);
+    text-underline-offset: 1em;
+    font-weight: 700;
+  }
+
+
+</style>
+
+
+
+
+
+
