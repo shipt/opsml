54891724f4468255b7f413a935b3a48d226ca2b6
Thorrester <steven.forrester@shipt.com>
2024-03-19 17:42:07 -0400

    update tests

--- tests/test_interface/test_model_interface_saver_loader_api.py
+++ tests/test_interface/test_model_interface_saver_loader_api.py
@@ -73,6 +73,7 @@ def test_save_huggingface_modelcard_api_client(
     loaded_card = cast(ModelCard, loader.load_card())
     assert isinstance(loaded_card, ModelCard)
 
+    assert isinstance(loaded_card.interface, HuggingFaceModel)
     loaded_card.load_model(load_preprocessor=True)
     assert type(loaded_card.interface.model) == type(modelcard.interface.model)
     assert type(loaded_card.interface.tokenizer) == type(modelcard.interface.tokenizer)
@@ -85,6 +86,7 @@ def test_save_huggingface_modelcard_api_client(
     loaded_card.load_onnx_model(load_quantized=True)
     assert loaded_card.interface.onnx_model is not None
     assert loaded_card.interface.onnx_model.sess is not None
+    assert loaded_card.interface.onnx_args.config is not None
 
     with tempfile.TemporaryDirectory() as tempdir:
         path = Path(tempdir)
