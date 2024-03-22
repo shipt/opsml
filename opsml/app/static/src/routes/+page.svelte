9755c8cbc1a6c094b971a8fcb4bd928adb252b96
Thorrester <steven.forrester@shipt.com>
2024-03-22 15:03:25 -0400

    coverage config

--- opsml/app/static/src/routes/+page.svelte
+++ opsml/app/static/src/routes/+page.svelte
@@ -1,88 +1,13 @@
 
 
 <script lang="ts">
+
   import logo from '$lib/images/opsml-logo.png';
-  import modelcard from '$lib/images/modelcard-circuit.svg';
-  import  { getRecentCards } from '$lib/scripts/homepage';
-  import HomeSpan from '$lib/Homepage_span.svelte';
-  import Card from '$lib/Card.svelte';
-	
-  interface CardRequest {
-  registry_type: string;
-  limit: number;
-  }
-
-  const cards = getRecentCards();
-
+  import Homepage from '$lib/Homepage.svelte';
 
 </script>
   
-<div id="active-page">
-  
-  <div class="container mx-auto mb-4 pt-12 sm:mb-4 sm:pt-20">
-    <div class="mb-10 flex items-center justify-center gap-2 text-xl font-bold sm:mb-8">
-        <div class="mr-2 h-px flex-1 translate-y-px bg-gradient-to-l from-primary-500 to-white"></div>
-          Recent <img src={logo} class="w-12" alt=""> Assets
-        <div class="ml-2 h-px flex-1 translate-y-px bg-gradient-to-r from-primary-500 to-white"></div>
-    </div>
-
-  
-    <div class="relative grid grid-cols-1 gap-6 lg:grid-cols-3">
-
-      <HomeSpan logo={modelcard} header="ModelCards" >
-        {#await cards}
-          <div>Loading...</div>
-        {:then cards}
-          {#each cards.modelcards as modelcard}
-            <Card 
-              hoverColor="hover:text-primary-600 dark:hover:text-primary-300"
-              repository= {modelcard.repository} 
-              name= {modelcard.name}
-              version= {modelcard.version}
-              date= {modelcard.date}
-              svgClass="flex-none w-3 mr-0.5 fill-primary-600 dark:fill-primary-200"
-            />
-          {/each}
-        {/await}
-      </HomeSpan>
-
-      <HomeSpan logo={modelcard} header="DataCards" >
-        {#await cards}
-          <div>Loading...</div>
-        {:then cards}
-          {#each cards.datacards as datacard}
-            <Card 
-              hoverColor=hover:text-secondary-600
-              svgClass="flex-none w-3 mr-0.5 fill-secondary-600"
-              repository= {datacard.repository} 
-              name= {datacard.name}
-              version= {datacard.version}
-              date= {datacard.date}
-            />
-          {/each}
-        {/await}
-      </HomeSpan>
-
-      <HomeSpan logo={modelcard} header="RunCards" >
-        {#await cards}
-          <div>Loading...</div>
-        {:then cards}
-          {#each cards.runcards as runcard}
-            <Card 
-              hoverColor=hover:text-error-600
-              svgClass="flex-none w-3 mr-0.5 fill-error-600"
-              repository= {runcard.repository} 
-              name= {runcard.name}
-              version= {runcard.version}
-              date= {runcard.date}
-            />
-          {/each}
-        {/await}
-      </HomeSpan>
-
-    </div>
-  </div>
-</div>
+<Homepage logo={logo} />
 
 
 		
