37d1de36f44626a2b280e5d9423695272df43965
Thorrester <steven.forrester@shipt.com>
2024-03-25 10:21:42 -0400

    add active class to nav

--- opsml/app/static/src/routes/opsml/registry/[type=registry]/+layout.svelte
+++ opsml/app/static/src/routes/opsml/registry/[type=registry]/+layout.svelte
@@ -28,19 +28,5 @@
 </script>
 
 <div class="container relative flex flex-col lg:grid lg:space-y-0 w-full lg:grid-cols-10 md:flex-1 md:grid-rows-full md:gap-6 ">
-  <section class="pt-8 border-gray-100 bg-white lg:static lg:px-0 lg:col-span-4 xl:col-span-3 lg:border-r lg:bg-gradient-to-l from-gray-50-to-white hidden lg:block ">
-    <div class="mb-4 items-center space-y-3 md:flex md:space-y-0 lg:mb-6">
-      <div class="flex items  -center text-lg">
-        <h1>Repositories</h1>
-      </div>
-      <div class="mb-20 lg:mb-4">
-        <div class="mb-4 flex items-center justify-between lg:mr-8">
-          <div class="relative flex min-w-0 flex-1 items-center">
-            <Search bind:searchTerm on:input={searchRepos} />
-          </div> 
-        </div>
-      </div>
-    </div>
-  </section>
   <slot></slot>
 </div>
