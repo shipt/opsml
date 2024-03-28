c45a8b23d3080472138fdd7e4340c35483a54733
Thorrester <steven.forrester@shipt.com>
2024-03-28 15:38:46 -0400

    routing

--- opsml/app/static/src/routes/opsml/registry/[registry]/versions/+page.svelte
+++ opsml/app/static/src/routes/opsml/registry/[registry]/versions/+page.svelte
@@ -1,3 +1,13 @@
-<div>
-    <h1>Artifacts</h1>
+<script>
+
+  import { TabGroup, Tab } from '@skeletonlabs/skeleton';
+
+</script>
+
+<div class="flex">
+  <div class="flex-auto w-64 p-4 bg-white dark:bg-surface-900 min-h-screen pr-16 ...">
+    <div class="flex flex-row items-center text-lg font-bold">
+      <h1>Artifacts</h1>
+    </div>
+  </div>
 </div>
