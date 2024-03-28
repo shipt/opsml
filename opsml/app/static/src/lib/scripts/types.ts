d2b5bc9ddc60cfb31daa6ffbeab2d04e83b58323
Thorrester <steven.forrester@shipt.com>
2024-03-28 17:38:57 -0400

    add button

--- opsml/app/static/src/lib/scripts/types.ts
+++ opsml/app/static/src/lib/scripts/types.ts
@@ -13,3 +13,30 @@ export interface registryStats {
 export interface repositories {
   repositories: string[];
 }
+
+export interface metadataRequest {
+  repository: string;
+  name: string;
+  version?: string;
+}
+
+export interface ModelMetadata {
+  model_name: string;
+  model_class: string;
+  model_type: string;
+  model_interface: string;
+  onnx_uri: string;
+  onnx_version: string;
+  model_uri: string;
+  model_version: string;
+  model_repository: string;
+  opsml_version: string;
+  data_schema: string;
+  preprocessor_uri?: string;
+  preprocessor_name?: string;
+  quantized_model_uri?: string;
+  tokenizer_uri?: string;
+  tokenizer_name?: string;
+  feature_extractor_uri?: string;
+  feature_extractor_name?: string;
+}
--- opsml/app/static/src/routes/opsml/registry/[registry]/versions/+page.ts
+++ opsml/app/static/src/routes/opsml/registry/[registry]/versions/+page.ts
@@ -1,9 +1,29 @@
+import { type metadataRequest, type ModelMetadata } from "$lib/scripts/types";
+
 /** @type {import('./$types').PageLoad} */
 export async function load({ params, url }) {
-  let name = url.searchParams.get("name");
-  let repository = url.searchParams.get("repository");
+  let name: string = url.searchParams.get("name")!;
+  let repository: string = url.searchParams.get("repository")!;
+  let version = url.searchParams.get("version");
   let registry = params.registry.replace(/s+$/, "");
   let path = params.registry;
+
+  // get model metadata
+  let metaAttr: metadataRequest = {
+    name: name,
+    repository: repository,
+  };
+
+  if (version) {
+    metaAttr["version"] = version;
+  }
+
+  const res: ModelMetadata = await fetch("/opsml/models/metadata", {
+    method: "POST",
+    body: JSON.stringify(metaAttr),
+  }).then((res) => res.json());
+
+  console.log(res);
 
   return {
     registry: registry,
